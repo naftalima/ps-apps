cba686d844e9f2ab24d8a6d186d4d35abdc839f4
naftalima <npl@cin.ufpe.br>
2020-03-20 11:18:32 -0300

    customfunctions

---
+++ customfunctions.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+def deleteSomeColumns(listColumns,df):
+    for i in listColumns:
+        df= df.drop(columns=i)
+    return df
+
+def categoryOnly(df, key):
+    category = []
+    for i in df['Category']:
+        if type(i) != float: #not NaN
+            if (key in i.upper() ) and (i not in category):
+                category.append(i)
+    return category
+
+def dfOnly(dfALL,categories):
+    column_names = list(dfALL.columns)
+    dfOnly = pd.DataFrame(columns = column_names)
+    for i in categories:
+        df = dfALL.loc[dfALL['Category'] == i] ## loc overwrite
+        dfOnly = dfOnly.append(df, ignore_index = True) 
+    return dfOnly
+
+def FreeGames(dfG):
+    df = dfG.loc[dfG['Price'] == '0']
+    return df
+
+def popularOrder(df):
+
+    df["Installs_clean"] = df["Installs"].str.replace("+","")     # df.Installs = df.Installs.apply(lambda x: x.strip("+"))
+    df["Installs_clean"] = df["Installs_clean"].str.replace(",","")     # df.Installs = df.Installs.apply(lambda x: x.replace(",",""))
+
+    df.drop(['Installs'], axis=1)
+
+    df.Installs = pd.to_numeric(df.Installs_clean)
+
+    df.drop(['Installs_clean'], axis=1)
+
+    dfSort = df.sort_values(by=['Installs'], ascending=False)
+
+    return(dfSort)
+
+def sizeRows(df):
+    size = df.size / df.columns.size
+    return(size)
